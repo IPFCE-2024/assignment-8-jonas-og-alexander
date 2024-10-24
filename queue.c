5eb3412b3cd7296c91bb85cca43647e8f2f3fbf4
Alexander Munk Andersen <Alex579b@gmail.com>
2024-10-24 15:26:48 +0200

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,26 +1,55 @@
+#include <stdio.h>
+#include <assert.h>
 #include "queue.h"
+#include <stdlib.h>
+#include <stdbool.h>
 
-
-// Opgave 3
 void initialize(queue *q) {
-
+    q->front = NULL;
+    q->rear = NULL;
+    q->size = 0;
 }
 
 bool empty(const queue *q) {
-    return false;
+    return q->size == 0;
 }
 
+
 bool full(const queue *q) {
-    return false;
+    return false; /*som sidst kan den aldrig være fuld da det er en linked list*/
 }
 
 void enqueue(queue *q, int x) {
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->data = x;
+    newNode->next = NULL;
 
+    if (q->rear == NULL) { /*hvis den var tom før*/
+        q->front = newNode;
+        q->rear = newNode;
+    } else {
+        q->rear->next = newNode;
+        q->rear =newNode;
+    }
+    q->size++;
 }
 
-int dequeue(queue *q) {
-    return -1;
+int dequeue(queue *q){
+    assert(q->size > 0);
+
+    node* temp = q->front;
+    int dequeued = temp->data;
+
+    q->front = q->front->next;
+
+    if (q-> front == NULL) { /*hvis den tømmes*/
+        q->rear = NULL;
+    }
+    free(temp);
+    q->size--;
+    return dequeued;
 }
+
 
 // Opgave 4
 void push(int element, node **head) {
